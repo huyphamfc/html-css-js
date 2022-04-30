3e0edb6deef70851fcb58a25fd7544dab36f6060
huyphamfc <huyphamfc@gmail.com>
2022-04-30 10:15:24 +0700

    Navigation Bar - update Script

--- 027--NavBar/script.js
+++ 027--NavBar/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const navbar = document.querySelector('.header__nav');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+    navbar.classList.toggle('nav--open');
+})
